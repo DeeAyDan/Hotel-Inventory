4d7728fdaefb3b2b192ce70b66498aaee956dd05
Daniel <83757480+DeeAyDan@users.noreply.github.com>
2023-07-31 23:49:27 +0200

    moved roomList into it's own component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -4,55 +4,59 @@ import { Room, RoomList } from './rooms';
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent {
-
-  hideRooms = false;
-
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
-    bookedRooms: 5, 
+    bookedRooms: 5,
   };
 
-  roomList : RoomList[] = [{
-    roomNumber : 1,
-    roomType : 'Deluxe Room',
-    amenities : 'Air Conditioner, Free Wi-Fi, TV, Bathrooms, Kitchen',
-    price : 1000,
-    photos : 'Placeholder for photo',
-    checkinTime : new Date('14-Aug-2023'),
-    checkoutTime : new Date('18-Aug-2023'),
-    rating : 4.5,
-  },
-  {
-    roomNumber : 2,
-    roomType : 'Standard Room',
-    amenities : 'Air Conditioner, TV, Bathrooms',
-    price : 500,
-    photos : 'Placeholder for photo',
-    checkinTime : new Date('16-Aug-2023'),
-    checkoutTime : new Date('20-Aug-2023'),
-    rating : 3.4,
+  hideRooms = false;
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = this.rooms.availableRooms;
 
-  },
-  {
-    roomNumber : 3,
-    roomType : 'Private Suite',
-    amenities : 'Air Conditioner, Free Wi-Fi, TV, Bathrooms, Kitchen',
-    price : 7500,
-    photos : 'Placeholder for photo',
-    checkinTime : new Date('21-Aug-2023'),
-    checkoutTime : new Date('23-Aug-2023'),
-    rating : 2.6,
+  roomList: RoomList[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooms, Kitchen',
+        price: 1000,
+        photos: 'Placeholder for photo',
+        checkinTime: new Date('14-Aug-2023'),
+        checkoutTime: new Date('18-Aug-2023'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Standard Room',
+        amenities: 'Air Conditioner, TV, Bathrooms',
+        price: 500,
+        photos: 'Placeholder for photo',
+        checkinTime: new Date('16-Aug-2023'),
+        checkoutTime: new Date('20-Aug-2023'),
+        rating: 3.4,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suite',
+        amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathrooms, Kitchen',
+        price: 7500,
+        photos: 'Placeholder for photo',
+        checkinTime: new Date('21-Aug-2023'),
+        checkoutTime: new Date('23-Aug-2023'),
+        rating: 2.6,
+      },
+    ];
   }
 
-]
-
-  hotelName = "Hilton Hotel";
-  numberOfRooms = this.rooms.availableRooms;
-  toggle(){
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 }
